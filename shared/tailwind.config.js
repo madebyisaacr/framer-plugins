706514d9eef83e4a287d802aa08086d139a5b54b
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-06-17 12:19:36 -0400

    Shared tailwind.config.js

--- shared/tailwind.config.js
+++ shared/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
-		"./plugins/*/index.html",
-		"./plugins/*/src/**/*.{js,jsx,ts,tsx}",
-		"./shared/**/*.{js,jsx,ts,tsx}",
+		"./index.html",
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"../../shared/**/*.{js,jsx,ts,tsx}",
 	],
 	theme: {
 		extend: {},
