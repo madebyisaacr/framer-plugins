769c05f8266148a4c5f956660f35fda131da85f8
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-06-18 22:15:20 -0400

    Airtable UI changes

--- plugins/framersync/integrations/airtable/airtable.jsx
+++ plugins/framersync/integrations/airtable/airtable.jsx
@@ -8,21 +8,7 @@ export function AirtablePage({ openPage, closePage }) {
 			<BackButton onClick={closePage} />
 			<h1>Sync Airtable with Framer</h1>
 			<div className="flex-1"></div>
-			<Button primary onClick={() => openPage(ConnectPage)}>
-				Connect Airtable Account
-			</Button>
-		</div>
-	);
-}
-
-function ConnectPage({ closePage }) {
-	return (
-		<div className="flex flex-col size-full p-3 pt-0 gap-2">
-			<BackButton onClick={closePage} />
-			<h1 className="text-xl font-bold -mb-1 mt-1">Connect Airtable account</h1>
-			<p>Connect your Airtable account to get started.</p>
-			<div className="flex-1" />
-			<Button primary>Connect</Button>
+			<Button primary>Connect Airtable Account</Button>
 		</div>
 	);
 }
