f45584b271c1b208404bfbae6189877f5dea4c71
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-08-08 17:00:16 -0400

    Add AirtableSync plugin

---
+++ plugins/airtablesync/src/SelectDatabase.tsx
@@ -0,0 +1,92 @@
+import { GetDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { richTextToPlainText, useDatabasesQuery } from "./notion";
+import { FormEvent, useEffect, useState } from "react";
+import notionConnectSrc from "./assets/notion-connect.png";
+import { assert } from "./utils";
+import { ReloadIcon } from "./components/Icons";
+import { framer } from "framer-plugin";
+import Button from "@shared/Button";
+import classNames from "classnames";
+import { Spinner } from "@shared/spinner/Spinner";
+
+interface SelectDatabaseProps {
+	onDatabaseSelected: (database: GetDatabaseResponse) => void;
+}
+
+export function SelectDatabase({ onDatabaseSelected }: SelectDatabaseProps) {
+	const { data, refetch, isRefetching, isLoading } = useDatabasesQuery();
+	const [selectedDatabase, setSelectedDatabase] = useState<string | null>(null);
+
+	framer.showUI({ width: 750, height: 550 });
+
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+
+		assert(data);
+
+		const database = data.find((database) => database.id === selectedDatabase);
+		if (!database) {
+			setSelectedDatabase(null);
+			return;
+		}
+
+		onDatabaseSelected(database);
+	};
+
+	return (
+		<div className="flex flex-row gap-3 size-full px-3 pb-3">
+			<div className="flex flex-col gap-3 w-[280px]">
+				<img src={notionConnectSrc} className="rounded" />
+				<p>
+					Connect your databases: open a database in Notion, click the ... button in the top-right corner of the page, then pick
+					Connections → Connect to → Framer.
+				</p>
+			</div>
+			<div className="w-[1px] bg-divider" />
+			<div className="flex flex-col gap-2 flex-1 justify-between">
+				<div className="flex items-center justify-between">
+					<span>Select a Notion database to sync</span>
+					<button
+						className="w-[32px] bg-transparent flex items-center justify-center text-secondary"
+						type="button"
+						onClick={() => refetch()}
+					>
+						<ReloadIcon className={isRefetching || isLoading ? "animate-spin" : undefined} />
+					</button>
+				</div>
+				{isLoading ? (
+					<div className="flex flex-col items-center justify-center flex-1 gap-4">
+						<Spinner inline />
+						Loading Databases...
+					</div>
+				) : (
+					<div className="flex-1 flex flex-col">
+						{data?.map((database) => (
+							<NotionDatabaseButton
+								key={database.id}
+								databaseName={richTextToPlainText(database.title)}
+								selected={selectedDatabase === database.id}
+								onClick={() => setSelectedDatabase(selectedDatabase === database.id ? null : database.id)}
+							/>
+						))}
+					</div>
+				)}
+				<Button primary disabled={!selectedDatabase} onClick={handleSubmit}>
+					Next
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+function NotionDatabaseButton({ databaseName, selected, onClick }) {
+	return (
+		<label
+			htmlFor={databaseName}
+			className={classNames("p-2 cursor-pointer flex flex-row gap-2 items-center", selected && "bg-secondary rounded")}
+		>
+			<input type="checkbox" name="database" id={databaseName} checked={selected} onChange={onClick} />
+			{databaseName}
+		</label>
+	);
+}
---
+++ plugins/airtablesync/src/blocksToHTML.ts
@@ -0,0 +1,126 @@
+import { BlockObjectResponse, RichTextItemResponse } from "@notionhq/client/build/src/api-endpoints";
+import { assert } from "./utils";
+
+export function richTextToHTML(texts: RichTextItemResponse[]) {
+	return texts
+		.map(({ plain_text, annotations, href }) => {
+			let html = plain_text;
+
+			// Apply formatting based on annotations
+			if (annotations.bold) {
+				html = `<strong>${html}</strong>`;
+			}
+			if (annotations.italic) {
+				html = `<em>${html}</em>`;
+			}
+			if (annotations.strikethrough) {
+				html = `<s>${html}</s>`;
+			}
+			if (annotations.underline) {
+				html = `<u>${html}</u>`;
+			}
+
+			if (annotations.code) {
+				html = `<code>${html}</code>`;
+			}
+
+			if (annotations.color !== "default") {
+				const color = annotations.color.replace("_", "");
+				html = `<span style="color:${color}">${html}</span>`;
+			}
+
+			if (href) {
+				html = `<a href="${href}" target="_blank" rel="noopener noreferrer">${html}</a>`;
+			}
+
+			return html;
+		})
+		.join("");
+}
+
+export function blocksToHtml(blocks: BlockObjectResponse[]) {
+	if (!blocks) return "";
+
+	let htmlContent = "";
+
+	for (let i = 0; i < blocks.length; i++) {
+		const block = blocks[i];
+		assert(block);
+		const item = block[block.type];
+
+		switch (block.type) {
+			case "paragraph":
+				htmlContent += `<p>${richTextToHTML(item.rich_text)}${childrenToHtml(block)}</p>`;
+				break;
+			case "heading_1":
+				htmlContent += `<h1>${richTextToHTML(item.rich_text)}</h1>`;
+				break;
+			case "heading_2":
+				htmlContent += `<h2>${richTextToHTML(item.rich_text)}</h2>`;
+				break;
+			case "heading_3":
+				htmlContent += `<h3>${richTextToHTML(item.rich_text)}</h3>`;
+				break;
+			case "divider":
+				htmlContent += "<hr >";
+				break;
+			case "image":
+				switch (item.type) {
+					case "external":
+						htmlContent += `<img src="${item.external.url}" alt="${item.caption[0]?.plain_text}" />`;
+						break;
+					case "file":
+						htmlContent += `<img src="${item.file.url}" alt="${item.caption[0]?.plain_text}" />`;
+						break;
+				}
+				break;
+			case "bulleted_list_item":
+			case "numbered_list_item":
+			case "to_do":
+				const tag = block.type === "numbered_list_item" ? "ol" : "ul";
+
+				// Start the list if it's the first item of its type or the previous item isn't a list of the same type
+				if (i === 0 || blocks[i - 1].type !== block.type) htmlContent += `<${tag}>`;
+
+				htmlContent += `<li>${richTextToHTML(item.rich_text)}${childrenToHtml(block)}</li>`;
+
+				// If next block is not the same type, close the list
+				if (i === blocks.length - 1 || blocks[i + 1].type !== block.type) {
+					htmlContent += `</${tag}>`;
+				}
+				break;
+			case "code":
+				htmlContent += `<pre><code>${richTextToHTML(item.rich_text)}</code></pre>`;
+				break;
+			case "quote":
+				htmlContent += `<blockquote>${richTextToHTML(item.rich_text)}${childrenToHtml(block)}</blockquote>`;
+				break;
+			case "callout":
+				htmlContent += `<aside>${richTextToHTML(item.rich_text)}${childrenToHtml(block)}</aside>`;
+				break;
+			case "toggle":
+				htmlContent += `<p>${richTextToHTML(item.rich_text)}${childrenToHtml(block)}</p>`;
+				break;
+			case "bookmark":
+				htmlContent += `<a href="${item.url}" target="_blank" rel="noopener noreferrer">${richTextToHTML(item.caption)}</a>`;
+				break;
+			case "equation":
+				htmlContent += `<p>${item.expression}</p>`;
+				break;
+			default:
+				// TODO: More block types can be added here!
+				// video
+				break;
+		}
+	}
+
+	return htmlContent;
+}
+
+function childrenToHtml(block) {
+	if (!block.has_children) {
+		return "";
+	}
+
+	return "";
+}
