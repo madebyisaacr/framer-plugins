fd954f74b15ea07153088bad654b35eaa6c5a4a9
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-08-19 19:00:00 -0400

    Add Google Sheets Sync plugin

---
+++ plugins/googlesheetssync/src/main.tsx
@@ -0,0 +1,100 @@
+import "./globals.css";
+
+import React, { ReactNode, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { CenteredSpinner } from "./components/CenteredSpinner.tsx";
+import {
+	PluginContext,
+	PluginContextUpdate,
+	getPluginContext,
+	synchronizeDatabase,
+	isAuthenticated,
+	refreshAirtableToken,
+} from "./googlesheets.ts";
+
+import { framer } from "framer-plugin";
+import { logSyncResult } from "./debug.ts";
+import { ErrorBoundaryFallback } from "./components/ErrorBoundaryFallback.tsx";
+import { assert } from "./utils.ts";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("Root element not found");
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: false,
+		},
+	},
+});
+
+function shouldSyncImmediately(pluginContext: PluginContext): pluginContext is PluginContextUpdate {
+	if (pluginContext.type !== "update") return false;
+
+	if (!pluginContext.database) return false;
+	if (!pluginContext.slugFieldId) return false;
+	if (pluginContext.hasChangedFields) return false;
+
+	return true;
+}
+
+function renderPlugin(context: PluginContext, app: ReactNode) {
+	const root = document.getElementById("root");
+	if (!root) throw new Error("Root element not found");
+
+	framer.showUI({
+		width: 350,
+		height: context.isAuthenticated ? 370 : 340,
+	});
+
+	ReactDOM.createRoot(root).render(
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<div className="w-full flex flex-col overflow-auto flex-1 select-none">
+					<ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
+						<Suspense fallback={<CenteredSpinner />}>{app}</Suspense>
+					</ErrorBoundary>
+				</div>
+			</QueryClientProvider>
+		</React.StrictMode>
+	);
+}
+
+async function runPlugin() {
+	try {
+		if (isAuthenticated()) {
+			await refreshAirtableToken();
+		}
+
+		const pluginContext = await getPluginContext();
+		const mode = framer.mode;
+
+		if (mode === "syncManagedCollection" && shouldSyncImmediately(pluginContext)) {
+			assert(pluginContext.slugFieldId);
+
+			const result = await synchronizeDatabase(pluginContext.base, pluginContext.table, {
+				fields: pluginContext.collectionFields,
+				ignoredFieldIds: pluginContext.ignoredFieldIds,
+				lastSyncedTime: pluginContext.lastSyncedTime,
+				slugFieldId: pluginContext.slugFieldId,
+			});
+
+			logSyncResult(result);
+
+			await framer.closePlugin();
+			return;
+		}
+
+		renderPlugin(pluginContext, <App context={pluginContext} />);
+	} catch (error) {
+		const message = error instanceof Error ? error.message : String(error);
+		// framer.closePlugin("An unexpected error ocurred: " + message, {
+		// 	variant: "error",
+		// });
+	}
+}
+
+runPlugin();
