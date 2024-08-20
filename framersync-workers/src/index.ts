a899618293a3fa8c25726ab30e541e3ab4ef8182
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-08-19 20:52:43 -0400

    Convert backend to support multiple platforms

--- framersync-workers/src/index.ts
+++ framersync-workers/src/index.ts
@@ -1,38 +1,68 @@
 import { getHTMLTemplate } from './getHTMLTemplate';
 import { generateRandomId, generateAirtableChallengeParams } from './generateAirtableChallenge';
 
+enum Platform {
+	Notion = 'notion',
+	Airtable = 'airtable',
+	GoogleSheets = 'googleSheets',
+}
+
+enum Command {
+	Authorize = 'authorize',
+	Poll = 'poll',
+	Refresh = 'refresh',
+	Redirect = 'redirect',
+}
+
 async function handleRequest(request: Request, env: Env) {
 	const requestUrl = new URL(request.url);
 
 	const ACCESS_CONTROL_ORIGIN = { 'Access-Control-Allow-Origin': env.PLUGIN_URI };
+	const sections = requestUrl.pathname.replace(/^\/+|\/+$/g, '').split('/');
+
+	const platform = sections[0]; // notion, airtable, googleSheets
+	const command = (platform as Platform) ? sections[1] : sections[0]; // authorize, poll, refresh, or redirect
 
 	// Generate an authorization URL to login into the provider, and a set of
 	// read and write keys for retrieving the access token later on.
-	if (request.method === 'POST' && requestUrl.pathname.startsWith('/authorize/')) {
+	if (request.method === 'POST' && command === Command.Authorize) {
 		const readKey = generateRandomId(16);
-		const challengeParams = await generateAirtableChallengeParams();
-		const writeKey = challengeParams.state;
+		let writeKey;
+		let keyValueStoreData;
+		let authorizeUrl;
 
-		const authorizeParams = new URLSearchParams();
-		authorizeParams.append('client_id', env.CLIENT_ID);
-		authorizeParams.append('redirect_uri', env.REDIRECT_URI);
-		authorizeParams.append('response_type', 'code');
-		authorizeParams.append('scope', 'data.records:read schema.bases:read');
+		switch (platform) {
+			case Platform.Airtable:
+				const challengeParams = await generateAirtableChallengeParams();
+				writeKey = challengeParams.state;
 
-		authorizeParams.append('state', challengeParams.state);
-		authorizeParams.append('code_challenge', challengeParams.code_challenge);
-		authorizeParams.append('code_challenge_method', 'S256');
+				const authorizeParams = new URLSearchParams();
+				authorizeParams.append('client_id', env.AIRTABLE_CLIENT_ID);
+				authorizeParams.append('redirect_uri', getRedirectURI(env));
+				authorizeParams.append('response_type', 'code');
+				authorizeParams.append('scope', 'data.records:read schema.bases:read');
 
-		// Generate the login URL for the provider.
-		const authorizeUrl = new URL('https://airtable.com/oauth2/v1/authorize/');
-		authorizeUrl.search = authorizeParams.toString();
+				authorizeParams.append('state', challengeParams.state);
+				authorizeParams.append('code_challenge', challengeParams.code_challenge);
+				authorizeParams.append('code_challenge_method', 'S256');
 
-		await env.keyValueStore.put(`readKey:${writeKey}`, JSON.stringify({ readKey, challengeVerifier: challengeParams.code_verifier }), {
+				// Generate the login URL for the provider.
+				const authorizeUrlObject = new URL('https://airtable.com/oauth2/v1/authorize/');
+				authorizeUrlObject.search = authorizeParams.toString();
+				authorizeUrl = authorizeUrlObject.toString();
+
+				keyValueStoreData = JSON.stringify({ readKey, challengeVerifier: challengeParams.code_verifier });
+				break;
+			default:
+				throw new Error('Unsupported platform');
+		}
+
+		await env.keyValueStore.put(`readKey:${writeKey}`, keyValueStoreData, {
 			expirationTtl: 60,
 		});
 
 		const response = JSON.stringify({
-			url: authorizeUrl.toString(),
+			url: authorizeUrl,
 			readKey,
 		});
 
@@ -47,26 +77,33 @@ async function handleRequest(request: Request, env: Env) {
 	// Once the user has been authorized via login page, the provider will
 	// redirect them back this URL with an access code (not an access token) and
 	// the write key we stored in the state param.
-	if (request.method === 'GET' && requestUrl.pathname.startsWith('/redirect/')) {
+	if (request.method === 'GET' && command === Command.Redirect) {
 		const authorizationCode = requestUrl.searchParams.get('code');
 		const writeKey = requestUrl.searchParams.get('state');
-		const codeChallenge = requestUrl.searchParams.get('code_challenge');
-		const codeChallengeMethod = requestUrl.searchParams.get('code_challenge_method');
 
-		if (codeChallengeMethod !== 'S256') {
-			return new Response('Invalid code challenge method', {
-				status: 400,
-			});
+		switch (platform) {
+			case Platform.Airtable:
+				const codeChallenge = requestUrl.searchParams.get('code_challenge');
+				const codeChallengeMethod = requestUrl.searchParams.get('code_challenge_method');
+
+				if (codeChallengeMethod !== 'S256') {
+					return new Response('Invalid code challenge method', {
+						status: 400,
+					});
+				}
+
+				if (!codeChallenge) {
+					return new Response('Missing code challenge URL param', {
+						status: 400,
+					});
+				}
+				break;
+			default:
+				throw new Error('Unsupported platform');
 		}
 
 		if (!authorizationCode) {
 			return new Response('Missing authorization code URL param', {
-				status: 400,
-			});
-		}
-
-		if (!codeChallenge) {
-			return new Response('Missing code challenge URL param', {
 				status: 400,
 			});
 		}
@@ -85,27 +122,39 @@ async function handleRequest(request: Request, env: Env) {
 			});
 		}
 
-		const { readKey, challengeVerifier } = JSON.parse(storedValue);
+		const valueJson = JSON.parse(storedValue);
 
-		// Generate a new URL with the access code and client secret.
-		const tokenParams = new URLSearchParams();
-		tokenParams.append('client_id', env.CLIENT_ID);
-		tokenParams.append('client_secret', env.CLIENT_SECRET);
-		tokenParams.append('redirect_uri', env.REDIRECT_URI);
-		tokenParams.append('code', authorizationCode);
-		tokenParams.append('grant_type', 'authorization_code');
-		tokenParams.append('code_verifier', challengeVerifier);
+		const { readKey } = valueJson;
 
-		// This additional POST request retrieves the access token and expiry
-		// information used for further API requests to the provider.
-		const tokenResponse = await fetch('https://airtable.com/oauth2/v1/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				Authorization: getAuthorizationHeader(env),
-			},
-			body: tokenParams.toString(),
-		});
+		let tokenResponse;
+
+		switch (platform) {
+			case Platform.Airtable:
+				const { challengeVerifier } = JSON.parse(storedValue);
+
+				// Generate a new URL with the access code and client secret.
+				const tokenParams = new URLSearchParams();
+				tokenParams.append('redirect_uri', getRedirectURI(env));
+				tokenParams.append('code', authorizationCode);
+				tokenParams.append('client_id', env.AIRTABLE_CLIENT_ID);
+				tokenParams.append('client_secret', env.AIRTABLE_CLIENT_SECRET);
+				tokenParams.append('grant_type', 'authorization_code');
+				tokenParams.append('code_verifier', challengeVerifier);
+
+				// This additional POST request retrieves the access token and expiry
+				// information used for further API requests to the provider.
+				tokenResponse = await fetch('https://airtable.com/oauth2/v1/token', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+						Authorization: getAuthorizationHeader(env),
+					},
+					body: tokenParams.toString(),
+				});
+				break;
+			default:
+				throw new Error('Unsupported platform');
+		}
 
 		if (tokenResponse.status !== 200) {
 			return new Response(tokenResponse.statusText, {
@@ -127,7 +176,7 @@ async function handleRequest(request: Request, env: Env) {
 		});
 	}
 
-	if (request.method === 'POST' && requestUrl.pathname.startsWith('/poll/')) {
+	if (request.method === 'POST' && command === Command.Poll) {
 		const readKey = requestUrl.searchParams.get('readKey');
 
 		if (!readKey) {
@@ -160,7 +209,7 @@ async function handleRequest(request: Request, env: Env) {
 		});
 	}
 
-	if (request.method === 'POST' && requestUrl.pathname.startsWith('/refresh/')) {
+	if (request.method === 'POST' && command === Command.Refresh) {
 		const refreshToken = requestUrl.searchParams.get('refresh_token');
 
 		if (!refreshToken) {
@@ -172,14 +221,24 @@ async function handleRequest(request: Request, env: Env) {
 			});
 		}
 
-		const refreshResponse = await fetch('https://airtable.com/oauth2/v1/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				Authorization: getAuthorizationHeader(env),
-			},
-			body: `refresh_token=${refreshToken}&grant_type=refresh_token`,
-		});
+		let refreshResponse;
+
+		switch (platform) {
+			case Platform.Airtable:
+				if (platform === Platform.Airtable) {
+					refreshResponse = await fetch('https://airtable.com/oauth2/v1/token', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/x-www-form-urlencoded',
+							Authorization: getAuthorizationHeader(env),
+						},
+						body: `refresh_token=${refreshToken}&grant_type=refresh_token`,
+					});
+				}
+				break;
+			default:
+				throw new Error('Unsupported platform');
+		}
 
 		if (refreshResponse.status !== 200) {
 			return new Response(refreshResponse.statusText, {
@@ -230,7 +289,7 @@ export default {
 			return new Response(`😔 Internal error: ${message}`, {
 				status: 500,
 				headers: {
-					"Access-Control-Allow-Origin": env.PLUGIN_URI,
+					'Access-Control-Allow-Origin': env.PLUGIN_URI,
 				},
 			});
 		});
@@ -238,5 +297,9 @@ export default {
 };
 
 function getAuthorizationHeader(env) {
-	return `Basic ${Buffer.from(`${env.CLIENT_ID}:${env.CLIENT_SECRET}`).toString('base64')}`;
+	return `Basic ${Buffer.from(`${env.AIRTABLE_CLIENT_ID}:${env.AIRTABLE_CLIENT_SECRET}`).toString('base64')}`;
+}
+
+function getRedirectURI(env) {
+	return env.REDIRECT_URI.replace('{platform}', Platform.Airtable);
 }
