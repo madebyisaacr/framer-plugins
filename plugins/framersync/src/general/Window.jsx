132ef85b85d1f9ca1e6ba505171aa22bf067725e
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-08-28 22:28:16 -0400

    Add page opacity animation

--- plugins/framersync/src/general/Window.jsx
+++ plugins/framersync/src/general/Window.jsx
@@ -1,5 +1,6 @@
 import { framer } from "framer-plugin";
-import classNames from "classnames";
+import { motion } from "framer-motion";
+
 const pageSizes = {
 	Integrations: {
 		width: 500,
@@ -27,15 +28,26 @@ export default function Window({ page, className, children }) {
 	framer.showUI(pageSizes[page]);
 
 	return (
-		<div
+		<motion.div
 			className={className}
 			style={{
 				...pageSizes[page],
 				minWidth: pageSizes[page].width,
 				minHeight: pageSizes[page].height,
 			}}
+			initial={{
+				opacity: 0,
+			}}
+			animate={{
+				opacity: 1,
+			}}
+			transition={{
+				type: "tween",
+				ease: "easeInOut",
+				duration: 0.15,
+			}}
 		>
 			{children}
-		</div>
+		</motion.div>
 	);
 }
