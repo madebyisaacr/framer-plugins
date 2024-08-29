d00563f4dd0147e4248be97b33a3e1da1478bc3b
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-08-29 17:39:07 -0400

    Licence key validation

--- plugins/framersync/src/general/LemonSqueezy.jsx
+++ plugins/framersync/src/general/LemonSqueezy.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useContext, createContext } from "react";
+import { framer } from "framer-plugin";
 
 const checkoutURL = "https://store.framestack.co/buy/24b67220-4e17-478b-9a4a-9cbf0e2db171";
 
@@ -9,19 +10,63 @@ export function useLemonSqueezy() {
 }
 
 export function LemonSqueezyProvider({ children }) {
-	useEffect(() => {
-		if (window.hasOwnProperty("createLemonSqueezy")) {
-			window.createLemonSqueezy();
+	function openCheckout() {
+		window.open(checkoutURL, "_blank");
+	}
 
-			LemonSqueezy.Setup({
-				eventHandler: (event) => {
-					console.log(event);
-				},
-			});
+	async function validateLicenseKey() {
+		const licenseKey = await framer.getPluginData("lemonSqueezyLicenseKey");
+		const instanceId = await framer.getPluginData("lemonSqueezyInstanceId");
+
+		if (!licenseKey || !instanceId) return false;
+
+		const searchParams = new URLSearchParams({
+			license_key: licenseKey,
+			instance_id: instanceId,
+		});
+
+		const response = await fetch(`https://api.lemonsqueezy.com/v1/licenses/validate`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				Accept: "application/json",
+			},
+			body: searchParams.toString(),
+		});
+
+		const data = await response.json();
+
+		if (!data.valid) {
+			await framer.setPluginData("lemonSqueezyLicenseKey", null);
+			await framer.setPluginData("lemonSqueezyInstanceId", null);
 		}
-	}, []);
+
+		return data.valid;
+	}
+
+	async function activateLicenseKey(licenseKey) {
+		const response = await fetch(`https://api.lemonsqueezy.com/v1/licenses/activate`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				Accept: "application/json",
+			},
+			body: `license_key=${licenseKey}`,
+		});
+
+		const data = await response.json();
+
+		if (data.activated) {
+			await framer.setPluginData("lemonSqueezyLicenseKey", licenseKey);
+			await framer.setPluginData("lemonSqueezyInstanceId", data.instance.id);
+		}
+
+		return data.activated;
+	}
 
 	return (
-		<LemonSqueezyContext.Provider value={{ openCheckout }}>{children}</LemonSqueezyContext.Provider>
+		<LemonSqueezyContext.Provider value={{ openCheckout, validateLicenseKey, activateLicenseKey }}>
+			{children}
+		</LemonSqueezyContext.Provider>
 	);
 }
