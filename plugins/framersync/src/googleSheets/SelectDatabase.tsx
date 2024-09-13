19f9b008291932e8b446a3268031841508979071
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-09-13 00:42:24 -0400

    Improve Google Sheet selection

--- plugins/framersync/src/googleSheets/SelectDatabase.tsx
+++ plugins/framersync/src/googleSheets/SelectDatabase.tsx
@@ -3,6 +3,10 @@ import { useEffect, useRef, useState } from "react";
 import Button from "@shared/Button";
 import { openGooglePicker, getFullSheet, getSheetsList } from "./googleSheets";
 import Window from "../general/Window";
+import { GoogleSheetsLogo, FramerLogo } from "../assets/AppIcons";
+import BackButton from "../components/BackButton";
+import { Spinner } from "@shared/spinner/Spinner";
+import classNames from "classnames";
 
 const apiBaseUrl =
 	window.location.hostname === "localhost"
@@ -14,6 +18,7 @@ export function SelectDatabasePage() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [selectedSpreadsheetId, setSelectedSpreadsheetId] = useState(null);
 	const [sheets, setSheets] = useState([]);
+	const [selectedSheet, setSelectedSheet] = useState(null);
 	const pollIntervalRef = useRef(null);
 	const readKeyRef = useRef(null);
 
@@ -67,6 +72,13 @@ export function SelectDatabasePage() {
 		setIsLoading(false);
 	};
 
+	const onBackButtonClick = () => {
+		updatePluginContext({
+			integrationId: null,
+			integrationContext: null,
+		});
+	};
+
 	useEffect(() => {
 		return () => {
 			if (pollIntervalRef.current) {
@@ -76,37 +88,84 @@ export function SelectDatabasePage() {
 	}, []);
 
 	return (
-		<Window
-			page="SelectDatabase"
-			className="flex-col gap-2 p-3 size-full items-center justify-center"
-		>
-			{!selectedSpreadsheetId && (
-				<Button
-					primary
-					onClick={handleSelectSheet}
-					loading={isLoading}
-					disabled={isLoading}
-					className="w-[200px]"
-				>
-					Select a Google Sheet
-				</Button>
-			)}
-			{selectedSpreadsheetId && sheets?.length > 0 && (
-				<>
-					<h2 className="text-base font-bold">Select a sheet:</h2>
-					<ul className="list-none p-0">
-						{sheets.map((sheet) => (
-							<li key={sheet.properties.sheetId} className="mb-2">
-								<Button
-									onClick={() => handleSheetSelect(sheet.properties.sheetId)}
-									disabled={isLoading}
-								>
-									{sheet.properties.title}
-								</Button>
-							</li>
-						))}
-					</ul>
-				</>
+		<Window page="Authenticate" className="flex-col size-full overflow-hidden">
+			<div className="flex-1 flex-col gap-3 px-3 pb-3 size-full overflow-y-auto">
+				<BackButton onClick={onBackButtonClick} />
+				<h1 className="text-base font-bold text-primary">Select a Google Sheet</h1>
+				{selectedSpreadsheetId ? (
+					<>
+						<p>Select a sheet:</p>
+						<div className="flex-col p-1 relative flex-1 bg-secondary rounded">
+							<div className="absolute top-0 inset-x-2 h-px bg-divider-secondary" />
+							{sheets.length === 0 ? (
+								<div className="flex-row items-center justify-center flex-1 gap-2 min-h-6 text-secondary">
+									<Spinner inline />
+									Loading sheets...
+								</div>
+							) : (
+								sheets?.map((sheet) => (
+									<div
+										key={sheet.properties.sheetId}
+										className={classNames(
+											"rounded h-6 flex-row items-center px-2",
+											selectedSheet === sheet
+												? "bg-segmented-control text-accent dark:text-primary font-semibold segmented-control-shadow"
+												: "text-secondary font-medium"
+										)}
+										onClick={() => {
+											setSelectedSheet(sheet);
+										}}
+									>
+										{sheet.properties.title}
+									</div>
+								))
+							)}
+						</div>
+					</>
+				) : (
+					<>
+						<div className="w-full aspect-[1.8] rounded bg-secondary flex-row items-center justify-center gap-3">
+							<GoogleSheetsLogo size={50} />
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								className="text-tertiary"
+							>
+								<path d="M5 12l14 0" />
+								<path d="M13 18l6 -6" />
+								<path d="M13 6l6 6" />
+							</svg>
+							<FramerLogo size={42} />
+						</div>
+						<div className="flex-col items-center gap-2 flex-1 w-full">
+							{isLoading ? (
+								<span className="text-center max-w-[80%] block text-secondary text-balance flex-col justify-center items-center flex-1">
+									Select a Google Sheet and return to this page to finish setup.
+								</span>
+							) : (
+								<p>Select a Google Sheet to sync with your Framer CMS collection.</p>
+							)}
+						</div>
+						<Button primary onClick={handleSelectSheet} loading={isLoading} disabled={isLoading}>
+							Select a Google Sheet
+						</Button>
+					</>
+				)}
+			</div>
+			{selectedSpreadsheetId && (
+				<div className="p-3 relative">
+					<div className="absolute top-0 inset-x-3 h-px bg-divider" />
+					<Button primary onClick={handleSelectSheet} loading={isLoading} disabled={isLoading}>
+						Next: Configure Collection Fields
+					</Button>
+				</div>
 			)}
 		</Window>
 	);
