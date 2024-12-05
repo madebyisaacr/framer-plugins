f94e472f48523c37cdb8e389c9105610818a607b
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2024-12-05 15:48:20 -0500

    Fix pin icon

--- plugins/iconstack/src/App.tsx
+++ plugins/iconstack/src/App.tsx
@@ -317,7 +317,6 @@ function HomePage() {
 									height="18"
 									viewBox="0 0 24 24"
 									fill="currentColor"
-									className="absolute left-[6px] top-[6px]"
 								>
 									<path d="M15.113 3.21l.094 .083l5.5 5.5a1 1 0 0 1 -1.175 1.59l-3.172 3.171l-1.424 3.797a1 1 0 0 1 -.158 .277l-.07 .08l-1.5 1.5a1 1 0 0 1 -1.32 .082l-.095 -.083l-2.793 -2.792l-3.793 3.792a1 1 0 0 1 -1.497 -1.32l.083 -.094l3.792 -3.793l-2.792 -2.793a1 1 0 0 1 -.083 -1.32l.083 -.094l1.5 -1.5a1 1 0 0 1 .258 -.187l.098 -.042l3.796 -1.425l3.171 -3.17a1 1 0 0 1 1.497 -1.26z" />
 								</svg>
@@ -332,7 +331,6 @@ function HomePage() {
 									strokeLinecap="round"
 									strokeLinejoin="round"
 									fill="none"
-									className="absolute left-[6px] top-[6px]"
 								>
 									<path d="M15 4.5l-4 4l-4 1.5l-1.5 1.5l7 7l1.5 -1.5l1.5 -4l4 -4" />
 									<path d="M9 15l-4.5 4.5" />
